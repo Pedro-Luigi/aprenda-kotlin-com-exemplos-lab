0598cdbf089861702d35352dc1ffb139ed31d059
Pedro Rezende <68929840+Pedro-Luigi@users.noreply.github.com>
2022-12-26 11:42:32 -0300

    Minha solução para o desafio.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,51 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+import kotlinx.coroutines.*
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+//Adicionei nome ao usuário;
+data class Usuario(val nome:String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+//Coloquei o nome como val ao invés de var e tirei o valor padrão de duração do curso;
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+//Mudei de data class para class e mudei conteudos de var para val;
+class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    //Adicionei vararg em usuário para receber vários usuários;
+    fun matricular(vararg usuario: Usuario) {
+        
+        //Enquanto tiver usuário, adicione o usuário em inscritos;
+        usuario.forEach{
+           inscritos.add(it) 
+        }
+        runBlocking{ visualizar() }
+        // TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    }
+    
+    //Criei uma função para exibir o curso e os alunos matriculados
+    suspend fun visualizar() = coroutineScope {
+        var duracaoBootCamp = 0
+        launch{
+            delay(1000L)
+            println("Os alunos ${inscritos.map{it.nome}.joinToString()} estão matriculados no curso.")
+        }
+        val cursos = conteudos.forEach{ duracaoBootCamp += it.duracao }
+        println("A formação $nome tem $duracaoBootCamp horas e tem os cursos de: ${conteudos.map{it.nome}.joinToString()}")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val conteudos:List<ConteudoEducacional> = listOf(ConteudoEducacional("POO", 8),
+                          							ConteudoEducacional("Arquiteturas", 4),
+                          							ConteudoEducacional("Avançando com Kotlin", 3))
+    
+       Formacao("Kotlin Experience", conteudos).matricular(Usuario("Pedro"),
+                                                        Usuario("Lucas"),
+                                                        Usuario("Cris"),
+                                                        Usuario("Tulio"))  
 }
